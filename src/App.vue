e4e38b592fb143e92e001b3b4ce67879ea92016f
Alex Simisi Nta <smsxander@gmail.com>
2024-02-11 14:53:58 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="./assets/logo.svg" alt="logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
