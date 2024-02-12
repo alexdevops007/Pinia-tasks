f669fe9d16ff6702f61d5fbdf76e9f044d5bf3fd
Alex Simisi Nta <smsxander@gmail.com>
2024-02-12 02:33:33 +0100

    create task details components

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,26 @@
       <img src="./assets/logo.svg" alt="logo" />
       <h1>Pinia Tasks</h1>
     </header>
+
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import { useTaskStore } from "./store/Taskstore";
+import TaskDetails from "./components/TaskDetails.vue";
+
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore();
+
+    return { taskStore };
+  },
+};
 </script>
